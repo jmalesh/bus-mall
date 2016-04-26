42252de5f2ed411736c8f159041c527149065a47
Jess Malesh <jamalesh84@gmail.com>
2016-04-25 20:54:35 -0700

    able to generate first random image

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-alert('Test');
+var randomImg = [];
+
+var addRight = document.getElementById('add-right');
+var addCenter = document.getElementById('add-center');
+var addLeft = document.getElementById('add-left');
+var showImg = document.getElementById('show-image');
+//Img constructor function
+function Picture(imgName, filePath) {
+  this.imgName = imgName;
+  this.filePath = filePath;
+  this.newPath = 'img/' + this.filePath + '.jpg>';
+  this.timesShown = 0;
+  this.timesClicked = 0;
+  //randomImg.push(this);
+}
+
+//pushing in array
+randomImg.push(new Picture('bag','bag'));
+randomImg.push(new Picture('banana', 'banana'));
+randomImg.push(new Picture('bathroom', 'bathroom'));
+randomImg.push(new Picture('boots', 'boots'));
+randomImg.push(new Picture('breakfast', 'breakfast'));
+randomImg.push(new Picture('bubblegum', 'bubblegum'));
+randomImg.push(new Picture('chair', 'chair'));
+randomImg.push(new Picture('cthulhu', 'cthulhu'));
+randomImg.push(new Picture('dogDuck', 'dogDuck'));
+randomImg.push(new Picture('dragon', 'dragon'));
+randomImg.push(new Picture('pen', 'pen'));
+randomImg.push(new Picture('petSweep', 'petSweep'));
+randomImg.push(new Picture('scissors', 'sissors'));
+randomImg.push(new Picture('tauntaun', 'tauntaun'));
+randomImg.push(new Picture('unicorn', 'unicorn'));
+randomImg.push(new Picture('usb', 'usb'));
+randomImg.push(new Picture('waterCan', 'waterCan'));
+randomImg.push(new Picture('wineGlass', 'wineGlass'));
+
+var tryThis;
+
+function doMath() {
+  return Math.floor(Math.random() * randomImg.length);
+}
+
+var index = doMath(Picture);
+
+var liEl = document.createElement('li');
+liEl.innerHTML = '<img src=' + randomImg[index].newPath;
+showImg.appendChild(liEl);
+
+// var li2 = document.createElement('li');
+// li2.innerHTML = '<img src=' + pic[index2].path + ' />';
+// appendImg2.appendChild(li2);
+
+doMath();
